cfa2588eef7121e7e8f9d8eb9778f35f0e08acc0
Adjobo Elie <120084291+Maelabad@users.noreply.github.com>
2025-02-12 22:21:56 +0000

    Add files via upload

---
+++ days.py
@@ -0,0 +1,43 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'première colonne': [1, 2, 3, 4],
+     'deuxième colonne': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Ci-dessous se trouve un DataFrame :', df, 'Ci-dessus se trouve un DataFrame.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+st.write("Un nouveau dashhh  : ")
+
+gph = plt.scatter(np.array([1, 2, 3, 4]), np.array([10, 20, 30, 40]) )
+
+st.write(gph)
+
